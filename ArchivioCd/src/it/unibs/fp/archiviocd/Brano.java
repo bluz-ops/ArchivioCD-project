ba1bdaac8fc86a3e4163023553880483274a9809
bluz-ops <b.molla@studenti.unibs.it>
2023-05-09 15:16:18 +0200

    Classe Brano completo

--- ArchivioCd/src/it/unibs/fp/archiviocd/Brano.java
+++ ArchivioCd/src/it/unibs/fp/archiviocd/Brano.java
@@ -4,6 +4,24 @@ package it.unibs.fp.archiviocd;
  * @author Fatih Aya
  *
  */
-public class Brano {
+	/**
+	 * 
+	 * @author Fatih Aya
+	 *
+	 */
+	public class Brano {
 
+		private String titolo;
+		private double durata;
+		public Brano(String titolo, double durata) {
+		
+			this.titolo = titolo;
+			this.durata = durata;
+		}
+		@Override
+		public String toString(){
+			StringBuffer descrizione = new StringBuffer();
+			descrizione.append("Titolo del Brano:"+titolo+ "Durata:" +durata );
+			return descrizione.toString();
+		}
 }
